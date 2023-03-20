6b74d9c0c7c834c290069f741a1207a226e441f8
Shuqi Liang <cheskaqiqi@gmail.com>
2023-03-20 16:52:40 -0400

    t1092: add tests for `git diff-files`
    Before integrating the 'git diff-files' builtin
    with the sparse index feature, add tests to
    t1092-sparse-checkout-compatibility.sh to ensure it currently works
    with sparse-checkout and will still work with sparse index
    after that integration.
    
    When adding tests against a sparse-checkout
    definition, we test two modes: all changes are
    within the sparse-checkout cone and some changes are outside
    the sparse-checkout cone.
    
    In order to have staged changes outside of
    the sparse-checkout cone, make a directory called 'folder1' and
    copy `a` into 'folder1/a'. 'folder1/a' is identical to `a` in the base
    commit. These make 'folder1/a' in the index, while leaving it outside of
    the sparse-checkout definition. Test 'folder1/a'being present on-disk
    without modifications, then change content inside 'folder1/a' in order
    to test 'folder1/a' being present on-disk with modifications.
    
    Signed-off-by: Shuqi Liang <cheskaqiqi@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- t/t1092-sparse-checkout-compatibility.sh
+++ t/t1092-sparse-checkout-compatibility.sh
@@ -2055,4 +2055,46 @@ test_expect_success 'grep sparse directory within submodules' '
 	test_cmp actual expect
 '
 
+test_expect_success 'diff-files with pathspec inside sparse definition' '
+	init_repos &&
+
+	write_script edit-contents <<-\EOF &&
+	echo text >>"$1"
+	EOF
+
+	run_on_all ../edit-contents deep/a &&
+
+	test_all_match git diff-files &&
+
+	test_all_match git diff-files deep/a &&
+
+	# test wildcard
+	test_all_match git diff-files deep/*
+'
+
+test_expect_failure 'diff-files with pathspec outside sparse definition' '
+	init_repos &&
+
+	test_sparse_match test_must_fail git diff-files folder2/a &&
+
+	write_script edit-contents <<-\EOF &&
+	echo text >>"$1"
+	EOF
+
+	# Add file to the index but outside of cone for sparse-checkout cases.
+	# Add file to the index without sparse-checkout cases to ensure all have
+	# same output.
+	run_on_all mkdir folder1 &&
+	run_on_all cp a folder1/a &&
+
+	# file present on-disk without modifications
+	test_all_match git diff-files &&
+	test_all_match git diff-files folder1/a &&
+
+	# file present on-disk with modifications
+	run_on_all ../edit-contents folder1/a &&
+	test_all_match git diff-files &&
+	test_all_match git diff-files folder1/a
+'
+
 test_done
